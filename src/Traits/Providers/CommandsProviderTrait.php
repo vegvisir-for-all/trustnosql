8f220e1972e2b5f352944ae1826b689164ef294a
Vegvisir Sp. z o.o <vegvisir@vegvisir.pl>
2018-12-16 00:26:53 +0100

    Delete role command ready

--- src/Traits/Providers/CommandsProviderTrait.php
+++ src/Traits/Providers/CommandsProviderTrait.php
@@ -16,8 +16,13 @@ trait CommandsProviderTrait
             return new \Vegvisir\TrustNoSql\Commands\Role\Create();
         });
 
+        $this->app->singleton('command.trustnosql.role.delete', function () {
+            return new \Vegvisir\TrustNoSql\Commands\Role\Delete();
+        });
+
         $this->commands([
-            'command.trustnosql.role.create'
+            'command.trustnosql.role.create',
+            'command.trustnosql.role.delete'
         ]);
     }
 
