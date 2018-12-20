f2acc39b64227a329147724cb608fae63f76ffd1
Vegvisir Sp. z o.o <vegvisir@vegvisir.pl>
2018-12-20 15:36:39 +0100

    Works on base middleware and logic parser

--- src/Middleware/BaseMiddleware.php
+++ src/Middleware/BaseMiddleware.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Redirect;
+use Vegvisir\TrustNoSql\Middleware\Parser\LogicParser;
 
 class BaseMiddleware {
 
@@ -98,54 +99,111 @@ class BaseMiddleware {
 
     protected function authorization($instruction)
     {
-        return !Auth::guest() &&
-            $this->parseInstruction($instruction, Auth::user());
+        dd(Auth::guest() &&
+            $this->parseInstruction($instruction, Auth::user()));
     }
 
     protected function parseInstruction($instruction, $user)
     {
 
+        $result = LogicParser::parseLogicString($instruction);
+
+        die();
+
         $instruction = preg_replace('/([A-Za-z*]{1,}:[A-Za-z*\/]{1,})/im', '(\1)', preg_replace('/\s+/', '', $instruction));
 
         foreach([
-            '|' => ' || ',
-            '&' => ' && '
+            '|' => '||',
+            '&' => '&&'
         ] as $from => $to) {
             $instruction = str_replace($from, $to, $instruction);
         }
 
-        $hasRole = function ($roleName) use ($user) {
+        $user = \App\User::first();
+
+        $hasRole = function ($roleName, $user) {
             return $user->hasRole($roleName);
         };
-        $hasRoleAndOwns = function ($roleName) use ($user) {
+        $hasRoleAndOwns = function ($roleName, $user) {
             return $user->hasRole($roleName);
         };
-        $hasPermission = function ($permissionName) use ($user) {
+        $hasPermission = function ($permissionName, $user) {
             return $user->hasRole($permissionName);
         };
-        $hasPermissionAndOwns = function ($permissionName) use ($user) {
+        $hasPermissionAndOwns = function ($permissionName, $user) {
             return $user->hasRole($permissionName);
         };
-        $hasTeam = function ($teamName) use ($user) {
+        $hasTeam = function ($teamName, $user) {
             return $user->hasTeam($teamName);
         };
 
-        /**
-         * Step 4
-         * Parsing to true/falses
-         */
-        $matches = [];
+        while(false !== strpos($instruction, '(') && false !== strpos($instruction, ')')) {
 
-        while(0 !== preg_match('/(\({1}[A-Za-z0-9*:\/]*\){1})/im', $instruction, $matches) && !empty($matches)) {
-            $partialExploded = explode(':', substr($matches[0], 1, strlen($matches[0]) - 2 ));
-            $instruction = str_replace($matches[0], (string) $${str_replace('*', 'AndOwns', 'has' . ucfirst($partialExploded[0]))}($partialExploded[1]) ? 'true' : 'false', $instruction);
+            /**
+             * Step 0
+             * Reduce basic brackets with middleware instructions
+             */
+            while(1 == preg_match('/\([A-Za-z0-9*]*\:[A-Za-z0-9*\/]*\)/im', $instruction, $matches)) {
+
+                $strResult = rand(0,1) == 1 ? 'true' : 'false';
+                $instruction = str_replace($matches[0], $strResult, $instruction);
+            }
+
+            /**
+             * Step 1
+             * Reduce basic brackets with single bool
+             */
+            while(false !== strpos($instruction, '(false)') || false !== strpos($instruction, '(true)')) {
+                $instruction = str_replace('(false)', 'false', $instruction);
+                $instruction = str_replace('(true)', 'true', $instruction);
+            }
+
+            /**
+             * Step 2
+             * Reduce bool&&bool and bool||bool
+             */
+            $matches = [];
+            while(1 == preg_match_all('/\((true|false)([\&\|]{2})(true|false)\)/im', $instruction, $matches)) {
+
+                foreach($matches[0] as $key => $fullString) {
+                    $result = $matches[2][$key] == '||'
+                        ? filter_var($matches[1][$key], FILTER_VALIDATE_BOOLEAN) || filter_var($matches[3][$key], FILTER_VALIDATE_BOOLEAN)
+                        : filter_var($matches[1][$key], FILTER_VALIDATE_BOOLEAN) && filter_var($matches[3][$key], FILTER_VALIDATE_BOOLEAN);
+
+                        $strResult = $result ? 'true' : 'false';
+
+                        $instruction = str_replace($fullString, $strResult, $instruction);
+                }
+            }
+
+            /**
+             * Step 3
+             * We must put all loose && operations into brackets
+             */
+            $instructionBefore = $instruction;
+            $instruction = preg_replace('/(true|false)([\&]{2})(true|false)/im', '(\1\2\3)', $instruction);
+
+            if($instructionBefore !== $instruction) {
+                // to first step
+                continue;
+            }
+
+            /**
+             * Step 3
+             * We must put first loose || operation into brackets
+             */
+            $instruction = preg_replace('/(true|false)([\|]{2})(true|false)/im', '(\1\2\3)', $instruction, 1);
+
+            if($instructionBefore !== $instruction) {
+                // to first step
+                continue;
+            }
+
+
+            break;
         }
 
-        eval('$result = (bool)(' . $instruction .');');
-
-        if(is_bool($result)) {
-            return $result;
-        }
+        return filter_var($instruction, FILTER_VALIDATE_BOOLEAN);
     }
 
 }
