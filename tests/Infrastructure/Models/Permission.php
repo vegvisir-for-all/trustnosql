33b2ba0a1452add5c0adf76e49b27f4ab6613549
Marek Ognicki <marek@taxi.edu.pl>
2018-12-21 03:08:08 +0000

    Updated automatically code

---
+++ tests/Infrastructure/Models/Permission.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Vegvisir\TrustNoSql\Tests\Infrastructure\Models;
+
+use Vegvisir\TrustNoSql\Models\Permission as TrustNoSqlPermission;
+
+class Permission extends TrustNoSqlPermission {}
---
+++ tests/Models/Models.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Vegvisir\TrustNoSql\Tests\Models;
+
+use Vegvisir\TrustNoSql\Tests\TestCase;
+
+class Models extends TestCase {}
