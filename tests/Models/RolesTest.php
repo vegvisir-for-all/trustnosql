42574b8bbd56aa2acc38355aedeb4f6d8020c4c2
Vegvisir Sp. z o.o <vegvisir@vegvisir.pl>
2018-12-26 13:23:40 +0100

    New roles test

--- tests/Models/RolesTest.php
+++ tests/Models/RolesTest.php
@@ -40,8 +40,8 @@ class RolesTest extends TestCase
 
             $role = Role::create($roleData);
 
-            $this->assertEquals($roleData['name'], $role->name);
-            $this->assertEquals($roleData['display_name'], $role->display_name);
+            $this->assertEqual($roleData['name'], $role->name);
+            $this->assertEqual($roleData['display_name'], $role->display_name);
         }
     }
 
@@ -71,16 +71,51 @@ class RolesTest extends TestCase
 
     public function testAttachingToUsers()
     {
+        $user = User::first();
+
+        $user->attachRole('admin');
+
+        $this->assertEqual(1, $user->roles()->where('name', 'admin')->count());
+
+        Role::create(['name' => 'superadmin']);
+
+        $user = User::last();
+
+        $user->attachRole('superadmin', 'manager');
+
+        $this->assertEqual(2, $user->roles()->count());
     }
 
     public function testDetachingFromUsers()
     {
+        $user = User::first();
 
+        $user->detachRole('admin');
+
+        $this->assertEqual(0, $user->roles()->where('name', 'admin')->count());
+
+        $user = User::last();
+
+        $user->detachRole('superadmin', 'manager');
+
+        $this->assertEqual(0, $user->roles()->count());
     }
 
     public function testHasRole()
     {
+        $user = User::first();
 
+        $user->attachRole('admin');
+
+        $this->assertTrue($user->hasRole('admin'));
+
+        $user->attachRole('superadmin');
+
+        $this->assertTrue($user->hasRole('admin,superadmin', true));
+
+        $this->assertTrue($user->hasRole('admin,manager', false));
+
+        $this->assertFalse($user->hasRole('admin,manager', true));
     }
 
     public function testHasRoleAliases()
