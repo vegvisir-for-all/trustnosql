59a310f1d265d8350c20a636c0cd9e76a73ec95d
Vegvisir Sp. z o.o <vegvisir@vegvisir.pl>
2019-01-03 04:10:11 +0100

    Added few grabbables for tests

---
+++ tests/Infrastructure/Grabbables/ModeBoth.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Vegvisir\TrustNoSql\Tests\Infrastructure\Grabbables;
+
+use Vegvisir\TrustNoSql\Models\Grabbable as TrustNoSqlGrabbable;
+
+class ModeBoth extends TrustNoSqlGrabbable
+{
+}
